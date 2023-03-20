9c99b9be2167e0710b1b2adaec4da3356f6a31ee
Shreya Maurya <100403765+shreyamaurya029@users.noreply.github.com>
2023-03-20 19:59:37 +0530

    Time: 7 ms (31.42%), Space: 7.7 MB (71.82%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -16,25 +16,3 @@ public:
         return fun(m-1, n-1, grid);
     }
 };
-
-
-
-/*class Solution {
-public:
-    int f(int i, int j,vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp){
-        if(i>0 && j>0 && obstacleGrid[i][j] == 1 ) return 0;
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = f(i-1, j,obstacleGrid,dp);
-        int right = f(i, j-1,obstacleGrid,dp);
-        return dp[i][j] = left+right;
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1, n-1,obstacleGrid, dp);
-    }
-};*/
